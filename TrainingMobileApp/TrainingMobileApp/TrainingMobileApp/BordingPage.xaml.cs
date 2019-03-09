ecc5894e6ee65aed105e0bda24d72cca02838eff
Seyed Emad Hosseini <irc.master@hotmail.com>
2019-03-09 16:59:36 +0330

    initial commit

---
+++ TrainingMobileApp/TrainingMobileApp/TrainingMobileApp/BordingPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrainingMobileApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BordingPage : ContentPage
+	{
+		public BordingPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
